b39f5cd141111f2998a50cdab5d8b5c13e182d1b
Siddharth Ram <siddharthram@gmail.com>
2013-04-14 18:48:30 -0700

    testing double submit

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 alert("Doc is ready!"+ $('#edit_task'));
-$('#edit_task').bind("ajax:success",function(evt,data,status,xhr) {
+$(".edit_task").bind("ajax:success",function(evt,data,status,xhr) {
 	var $form = $(this);
 	alert ("form is" + $form);
 	$form.bind('submit', function() {
-	var act = $('#edit_task').attr('action');
+	var act = $(".edit_task").attr('action');
 	alert ("action "+ act);
 	var portal = "http://ximly.herokuapp.com" + act;
-	var data = $('#exit_task').serialize();
+	var data = $('.edit_task').serialize();
 	$.ajax({ 
 		type: 'PUT',
 		url: portal,
@@ -17,9 +17,9 @@ $('#edit_task').bind("ajax:success",function(evt,data,status,xhr) {
   alert ("ok, done! " + response);
   //$(formtag).attr("action","http://workersandbox.mturk.com/mturk/externalSubmit");
   //$(formtag).submit(); 
-  $('#edit_task').unbind('submit');
-  $('#edit_task').attr("action","http://workersandbox.mturk.com/mturk/externalSubmit");
-  $('#edit_task').submit();
+  $('.edit_task').unbind('submit');
+  $('.edit_task').attr("action","http://workersandbox.mturk.com/mturk/externalSubmit");
+  $('.edit_task').submit();
   });
   req.fail(function(response, ts, error){
   alert("failed" +ts + error);
