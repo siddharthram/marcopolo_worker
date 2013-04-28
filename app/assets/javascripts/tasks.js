50fa06da4e9786447a0685140c4572614e19ece0
Siddharth Ram <siddharthram@gmail.com>
2013-04-28 16:05:37 -0700

    testing double submit

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -4,7 +4,6 @@ function sendToTurk() {
 $(".edit_task").bind("ajax:success",function(evt,data,status,xhr) {
 	var $form = $(this);
 	alert ("form is" + $form);
-	//$('.edit_task').bind('submit', function() {
 	var act = $(".edit_task").attr('action');
 	alert ("action "+ act);
 	var portal = act;
@@ -17,8 +16,6 @@ $(".edit_task").bind("ajax:success",function(evt,data,status,xhr) {
 
   req.done(function(response, ts, jqXHR) {
   alert ("ok, done! " + ts);
-  //$(formtag).attr("action","http://workersandbox.mturk.com/mturk/externalSubmit");
-  //$(formtag).submit(); 
   alert("type is" + $('.edit_task').attr("type"));
   $('.edit_task').attr("type", "post");
   $('.edit_task').attr("method","post");
@@ -30,29 +27,7 @@ $(".edit_task").bind("ajax:success",function(evt,data,status,xhr) {
   req.fail(function(response, ts, error){
   alert("failed" +ts + error);
 });
-
-});
 return false;
-//});	
-return;
-}
+});
 
-function doClick() {
-			var act = $(".edit_task").attr("action");
-			alert ("yo, put goes to " + act);
-			$.ajax({
-				type: 'PUT',
-				url : act,
-				data : $('.edit_task').serialize(),
-				success : function(response) {
-					//$('.edit_task').html(response);
-					//$('.edit_task').unbind('submit');
-	            	alert("submmiting to mturk from edit");
-	            	var ac = "http://workersandbox.mturk.com/mturk/externalSubmit";
-	            	alert ("ac is" + ac);
-	            	alert("calling mturk");
-					$(".edit_task").attr("action", ac);
-					$('.edit_task').submit();
-			}
-		});
-		}
+
