d4517b73cc97f59855cf914d5672d411fabd0c63
Siddharth Ram <siddharthram@gmail.com>
2013-04-28 18:34:04 -0700

    testing double submit

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -15,18 +15,18 @@ $(".edit_task").bind("ajax:success",function(evt,data,status,xhr) {
 	});
 
   req.done(function(response, ts, jqXHR) {
-  alert ("ok, done! " + ts);
-  alert("type is" + $('.edit_task').attr("type"));
   $('.edit_task').attr("type", "post");
   $('.edit_task').attr("method","post");
-
   $('.edit_task').unbind('submit');
   $('.edit_task').attr("action","http://workersandbox.mturk.com/mturk/externalSubmit");
   $('.edit_task').submit();
+
   });
+
+
   req.fail(function(response, ts, error){
-  alert("failed" +ts + error);
 });
+
 return false;
 });
 
