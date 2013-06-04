2db7adc25073fd53d82fce288ee2a48bbdb46ee7
Siddharth Ram <siddharthram@gmail.com>
2013-06-03 18:53:03 -0700

    added jquery-forms

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -180,7 +180,7 @@ def preview
     puts "assignement id = " + @assignment.to_s
     puts "task is" + params[:task].to_s
 
-    upload_file = params[:task].tempfile][0].to_path.to_s
+    upload_file = params[:task].tempfile[0].to_path.to_s
     #upload_file = File.new(@attachment, "rb")
     #File.open(Rails.root.join('public', 'uploads', @attachment.original_filename), 'w') do |file|
      # file.write(@attachment.read)  
