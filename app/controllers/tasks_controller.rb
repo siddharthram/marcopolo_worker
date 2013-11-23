fc3d65abe6de433c02b85b965db091ce8326a04d
Siddharth Ram <siddharthram@gmail.com>
2013-11-23 12:22:08 -0800

    rollback to old handling of mturk

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 
-class TasksController < ApplicationController
+  class TasksController < ApplicationController
   include TasksHelper
   #before_filter :authenticate_user!
   before_filter :authenticate_user!, :except => [:preview, :postview, :edit, :update]
@@ -18,33 +18,24 @@ class TasksController < ApplicationController
       if (page == nil )
         response = HTTParty.get(@@base + '/task/open')
         puts "response = " + response.to_s
-        #Task.delete_all
+        Task.delete_all
         newTasks = []
         #response.parsed_response.each do |k, v|
         
         statuses = response.parsed_response["taskStatuses"]
-        #
-        # For each response, check if the entry already exists in the local cache
-        # if not, add it - these are the new jobs since last check
-        # If yes, ignore - the task already exists
-
         statuses.each do |job|
           suri = job["serverUniqueRequestId"]
           img = job["imageUrl"]
           req = job["requestedResponseFormat"]
          # v.each do |a|
-         puts "" + job.to_s
-         if Task.exists?(xim_id: suri)
-              # do nothing
-            else
-              newTasks << Task.new(xim_id: suri, isturkjob: false, imageurl: img , attachmentformat: req)
-            end 
+            puts "" + job.to_s
+            newTasks << Task.new(xim_id: suri, isturkjob: false, imageurl: img , attachmentformat: req)     
             puts "SAVING T.... " + t.to_s
          # end
-       end
-       Task.import newTasks
-     end
-     puts "YO, TASK!!"
+        end
+        Task.import newTasks
+      end
+      puts "YO, TASK!!"
     #@tasks = Task.all
     @tasks = Task.paginate(:page => params[:page], :per_page => 5)
     respond_to do |format|
@@ -57,10 +48,7 @@ class TasksController < ApplicationController
   end
 end
 
-#============================================
-#Preview is called by Turk workers ONLY
-#so isturkjob is always set to true
-#============================================
+
 
 def preview
   puts "IN PREVIEW"
@@ -70,31 +58,20 @@ def preview
   @hit = params[:hitId]
   @worker = params[:workerId]
   @attachmentformat = params[:requestedResponseFormat]
-  @turktext = "Ignore all the graphics in this image - just type the text content"
 
   #puts "attachmentformat ==========> " + @attachmentformat
-  #@task = Task.new(xim_id: @server, imageurl: @imagelocation, isturkjob: true, attachmentformat: @attachmentformat)  
+  @task = Task.new(xim_id: @server, imageurl: @imagelocation, isturkjob: true, attachmentformat: @attachmentformat)  
   #puts "task id " + @task.id
 
   id = params[:id]
   if (@assignment != "ASSIGNMENT_ID_NOT_AVAILABLE")
     puts "mturk - read to work on task - " + @assignment.to_s
     #task has been accepted
-    # create new task IF it does not exist 
-    # if it already exists, we add additional data that it is assigned to turk and lock it during edit
-
-    if Task.exists?(xim_id: @server)
-      puts "Task @server already exists"
-      t = Task.find_by_xim_id(@server)
-      t.isturkjob = true
-      @turktext = "Create a powerpoint file with the diagram and text in it. You MUST submit a powerpoint file"
-      t.save
-    else
+      # add new task and then display it
       t = Task.new(xim_id: @server, isturkjob: true, attachmentformat: @attachmentformat, imageurl: @imagelocation)
-      t.save
-    end
-    redirect_to action: :edit, id: @server, attachmentformat: @attachmentformat, hitId: @hit, workerId: @worker, imageUrl: @imagelocation, assignmentId: @assignment, serverUniqueRequestId: @server
-  else 
+      t.save     
+      redirect_to action: :edit, id: @server, attachmentformat: @attachmentformat, hitId: @hit, workerId: @worker, imageUrl: @imagelocation, assignmentId: @assignment, serverUniqueRequestId: @server
+    else 
       #preview mode
       puts "=============PREVIEW MODE=================="
       render  :layout => 'noheader' #:layout => false # render the preivew with no layout
@@ -129,13 +106,13 @@ def preview
   def edit
     puts "---------====ENTERING EDIT=========----"
     @task = nil
-    email = ""
-    @server = params[:serverUniqueRequestId]
-    @assignment = params[:assignmentId]
-    @imagelocation = params[:imageUrl]
-    @worker = params[:workerId]
-    @hit = params[:hitId]
-    @attach = params[:attach]
+     email = ""
+  @server = params[:serverUniqueRequestId]
+  @assignment = params[:assignmentId]
+  @imagelocation = params[:imageUrl]
+  @worker = params[:workerId]
+  @hit = params[:hitId]
+  @attach = params[:attach]
   # testing
   #@format="ppt"
   @id = params[:id]
@@ -143,38 +120,34 @@ def preview
 
   puts "**********Task id is" + params[:id]  + @worker.to_s + @hit.to_s
  #xim_id = params[:id]
- if (current_user == nil)
-    #
-    # Then this is a mturk job since There is no user id.
-    # So we find the task using the xim_id
-    #  mturk job - look up by xim_id
-    #
+  if (current_user == nil)
+    #mturk job - look up by xim_id
     @task = Task.find_by_xim_id(@server)
+    puts 'imagelocation is' + @imagelocation
   else
-     #
-     # this is a local task ie done by a ximly worker
-     # from the worker portal
-     #
-     @task = Task.find_by_xim_id(params[:id])
-     email = current_user.email.to_s
-     xim_id= @task.xim_id.to_s
-     @imagelocation = @task.imageurl
-
-      #
+      @task = Task.find_by_xim_id(params[:id])
+      email = current_user.email.to_s
+      xim_id= @task.xim_id.to_s
+      @imagelocation = @task.imageurl
+      puts "===sending data " + xim_id + " " + email
       #Lock the task..
-      #Locking it makes it unavailable to other users
-      #
       options = {
-        :headers => {'Content-type' => 'application/x-www-form-urlencoded'},
-        :body => {
-          :serverUniqueRequestId => @task.xim_id,
-          :emailId => email
-        }
-      }
-      r = HTTParty.post(@@base + '/task/lock', options).inspect
-    end
+      :headers => {'Content-type' => 'application/x-www-form-urlencoded'},
+      :body => {
+        :serverUniqueRequestId => @task.xim_id,
+        :emailId => email
+       }
+     }
+     r = HTTParty.post(@@base + '/task/lock', options).inspect
+       #r = HTTParty.get(@@base + '/task/lock?' +  "serverUniqueRequestId=" + xim_id + "&emailId=" + email).inspect
+       puts "r= " + r.to_s
+     end
+
+    #@task.xim_id = params[:id].to_i
+    # FIXME - hard coded image for now
       render  :layout => 'noheader' #:layout => false # render the preivew with no layout
-    end
+
+  end
 
   # POST /tasks
   # POST /tasks.json
@@ -201,7 +174,7 @@ def preview
     puts "PARAMS============" + params.to_s
     @id = params[:id]
     @task = Task.find(params[:id])
-    puts "found task " + @task.to_s
+    #puts "found task " + @task.to_s
 
     @assignment = params[:assignmentId]
     @current_user = current_user
@@ -211,31 +184,16 @@ def preview
     @attachment = nil
     if (params[:task])
 
-      ofile = params[:task][:attachment].original_filename
-      puts "ofile is +" + ofile.to_s
-      @fileext = File.extname(ofile)
-      @fileext = @fileext.sub(/^\./,'')
-      if (@fileext == "ppt" || @fileext == "pptx")
-        #
-        # then we are good- do nothing
-        # 
-        @isppt = true
-      else
-        #
-        # return invalid response
-        #
-        puts "*****PPT is false"
-        @isppt = false
-      end
-
+    ofile = params[:task][:attachment].original_filename
+    puts "ofile is +" + ofile.to_s
+    @fileext = File.extname(ofile)
+    @fileext = @fileext.sub(/^\./,'')
     #upload_file = ""
-  end
-  if (params[:task] != nil )
-    #
-    # attachment
-    #
-    puts "Adding PPT attachment..."
-    @attachment = params[:task][:attachment]
+    end
+
+    if (params[:task] != nil )
+      puts "Adding PPT attachment..."
+      @attachment = params[:task][:attachment]
 
     @options = {
       #:headers => {'Content-type' => 'multipart/form-data'},
@@ -249,59 +207,112 @@ def preview
     }
 
   else
-    #
-    # it is not an attachment
-    #
     @options = {
       :body => {
         :serverUniqueRequestId => @task.xim_id,
         :output => @output,
       }
     }
-  end
+      #upload_file = File.read(params[:task][:attachment].tempfile.to_path).force_encoding("BINARY")
+    end
 
-  if ((@isppt == true) || (params[:task] == nil))
-  #
-  # either there is an attachment and it is ppt, or 
-  # there is plain text. Either case, submit.
-  #
-  r = HTTMultiParty.post(@@base + '/task/submit', @options).inspect
-  puts "submit response from server" + r
-  puts "turk job = " + @task.isturkjob.to_s
-  turkjob = @task.isturkjob
-  @task.destroy
-end
-if (turkjob == false)
+    #puts "attachment = " + @attachment
+    #puts "output is " + @output.to_s
+    #puts "server id" + @task.xim_id
+    #puts "attachment" + @attachment.tempfile.to_path.to_s
+    #puts "assignement id = " + @assignment.to_s
+    #puts "task is" + params[:task].to_s
+
+
+    #upload_file = File.new(@attachment, "rb")
+    #File.open(Rails.root.join('public', 'uploads', @attachment.original_filename), 'w') do |file|
+     # file.write(@attachment.read)  
+    #end
+    #puts "uploaded" + upload_file.to_s
+
+
+
+
+   # @mturk = {
+      #:headers => {'Content-type' => 'application/x-www-form-urlencoded'},
+    #  :body     => {
+    #    :assignmentId => @assignment,
+    #    :output => @output
+    #  }
+    #}
+
+    r = HTTMultiParty.post(@@base + '/task/submit', @options).inspect
+    puts "submit response from server" + r
+    puts "turk job = " + @task.isturkjob.to_s
+
+    if (@task.isturkjob == false)
     # redirect only if it is on the portal
     puts "sending to root_url"
     respond_to do |format|
       format.html { redirect_to root_url}
     end
   else 
-    #
-    # for mturks. returns the relative url because of the option
-    #:only_path - If true, returns the relative URL (omitting the protocol, host name, and port) (false by default).
-    #
     url_for(:only_path => true )
   end
+    #r = HTTParty.post("http://workersandbox.mturk.com/mturk/externalSubmit",@mturk).inspect
+    #puts "response from turk is " + r
+    #puts "response ======" + r.to_s
+    #getTasks
+    #puts "done with tasks.. getting the next one"
+    #puts "FIRST IS=====" + Task.first.to_s
+    
+#alert ("opt is " + @options);
+#$.post("http://default-environment-jrcyxn2kkh.elasticbeanstalk.com/task/submit",@options,function()
+ # {alert("posted to server")});
+#$(form).submit(data, function(d) { alert("retuned " + d) });
 
+    #puts "OUTPUT...." + output
+  #respond_to do |format|
+      #format.html { render action: "edit" }
+   #   format.js
+      #format.json { render json: @task.errors, status: :unprocessable_entity }
+    #end
+
+=begin
+    respond_to do |format|
+      puts "format ==== " + format.to_s
+       if @task.update_attributes(params[:task])
+        #format.html { redirect_to @task, notice: r.to_s}
+        #format.html { redirect_to root_url}
+        if (Task.first == nil)
+          puts "NOOOO MOOOORE Tasks"
+          render :file => "public/nomore.html"
+        else
+          puts "++++++++++editing " + Task.first.to_s
+          #format.html { redirect_to edit_task_path(Task.first.xim_id) }
+        #format.html {render action: "edit"}
+        format.js
+        format.json { head :no_content }
+      end
+    else
+      format.html { render action: "edit" }
+      format.js
+      format.json { render json: @task.errors, status: :unprocessable_entity }
+    end
+  end
+=end
 
 end
 
 def notify
   puts "--=----IN NOTIFY===-====="
-  @id = params[:id]
-  @task = Task.find(params[:id])
-  output = params[:output]
+    @id = params[:id]
+    @task = Task.find(params[:id])
+    output = params[:output]
 
-  puts "ID is " + @id.to_s+ " task is " + @task.to_s + " output is " + @output
+    puts "ID is " + @id.to_s+ " task is " + @task.to_s + " output is " + @output
   @options = {
-    :headers => {'Content-type' => 'application/x-www-form-urlencoded'},
-    :body => {
-      :serverUniqueRequestId => @task.xim_id,
-      :output => output
+      :headers => {'Content-type' => 'application/x-www-form-urlencoded'},
+      :body => {
+        :serverUniqueRequestId => @task.xim_id,
+        :output => output
+      }
     }
-  }
   r = HTTParty.post(@@base + '/task/submit', @options).inspect
   puts "submit response from server"
 end
@@ -319,6 +330,3 @@ end
     end
   end
 end
-
-
-
